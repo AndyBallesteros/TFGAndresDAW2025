1c4f8eb673d00563d4b6a151119a234cc4832dbf
Andres Ballesteros <andresballesteros1917@gmail.com>
2025-06-18 17:30:55 +0200

    feat: contacto

--- pea-frontend/api/send-email.js
+++ pea-frontend/api/send-email.js
@@ -1,6 +1,4 @@
 const nodemailer = require('nodemailer');
-// require('dotenv').config(); 
-
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT || '587', 10),
